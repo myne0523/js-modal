c2bdf9b70d9c29816190f1e48a10a5dc6301f808
Your Name <your-email@example.com>
2025-08-15 17:38:56 +0700

    Thêm tùy chọn destroyOnClose

--- main.js
+++ main.js
@@ -2,8 +2,12 @@ const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
 function Modal(options = {}) {
-    const { templateId, closeMethod = ["button", "overplay", "escape"] } =
-        options;
+    const {
+        templateId,
+        destroyOnClose = true,
+        cssClass = [],
+        closeMethod = ["button", "overplay", "escape"],
+    } = options;
     const template = $(`#${templateId}`);
 
     if (!template) {
@@ -35,15 +39,21 @@ function Modal(options = {}) {
         return scrollbarWidth;
     }
 
-    this.open = () => {
+    this._build = () => {
         const content = template.content.cloneNode(true);
 
         // Create modal elements
-        const backdrop = document.createElement("div");
-        backdrop.className = "modal-backdrop ";
+        this._backdrop = document.createElement("div");
+        this._backdrop.className = "modal-backdrop ";
 
         const container = document.createElement("div");
         container.className = "modal-container";
+
+        cssClass.forEach((className) => {
+            if (typeof className === "string") {
+                container.classList.add(className);
+            }
+        });
 
         if (this._allowButtonClose) {
             const closeBtn = document.createElement("button");
@@ -51,7 +61,7 @@ function Modal(options = {}) {
             closeBtn.innerHTML = "&times;";
 
             container.append(closeBtn);
-            closeBtn.onclick = () => this.close(backdrop);
+            closeBtn.onclick = () => this.close();
         }
 
         const modalContent = document.createElement("div");
@@ -60,11 +70,17 @@ function Modal(options = {}) {
         // Append content and elements
         modalContent.append(content);
         container.append(modalContent);
-        backdrop.append(container);
-        document.body.append(backdrop);
+        this._backdrop.append(container);
+        document.body.append(this._backdrop);
+    };
+
+    this.open = () => {
+        if (!this._backdrop) {
+            this._build();
+        }
 
         setTimeout(() => {
-            backdrop.classList.add("show");
+            this._backdrop.classList.add("show");
         }, 0);
 
         // Disable scrolling
@@ -74,9 +90,9 @@ function Modal(options = {}) {
         // Attack event listener
 
         if (this._allowBackdropClose) {
-            backdrop.onclick = (e) => {
-                if (e.target === backdrop) {
-                    this.close(backdrop);
+            this._backdrop.onclick = (e) => {
+                if (e.target === this._backdrop) {
+                    this.close();
                 }
             };
         }
@@ -84,23 +100,30 @@ function Modal(options = {}) {
         if (this._allowEscapeClose) {
             document.addEventListener("keydown", (e) => {
                 if (e.key === "Escape") {
-                    this.close(backdrop);
+                    this.close();
                 }
             });
         }
 
-        return backdrop;
+        return this._backdrop;
     };
 
-    this.close = (modalElement) => {
-        modalElement.classList.remove("show");
-        modalElement.ontransitionend = () => {
-            modalElement.remove();
+    this.close = (destroy = destroyOnClose) => {
+        this._backdrop.classList.remove("show");
+        this._backdrop.ontransitionend = () => {
+            if (this._backdrop && destroy) {
+                this._backdrop.remove();
+                this._backdrop = null;
+            }
 
             // Disable scrolling
             document.body.classList.remove("no-scroll");
             document.body.style.paddingRight = "";
         };
+    };
+
+    this.destroy = () => {
+        this.close(true);
     };
 }
 
